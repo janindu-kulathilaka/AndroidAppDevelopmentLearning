239d29e49f104b3482276751649cd9005e74bfcf
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-06-29 16:37:35 +0530

    add some button and connect with the function to disable it

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.test1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("fail", "it's not working");
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void toggle(View v) {
+        v.setEnabled(false);
+        Button button = (Button) v;
+        button.setText("Disabled");
+        Log.d("Sucess", "Button Disabled");
+
+        TextView text = findViewById(R.id.hello);
+
+        text.setText("hello janindu..");
+
+
     }
 }
